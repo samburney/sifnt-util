9f703d77f86b54c2f8090fd26af45613d4895070
Sam Burney <sam.burney@air-stream.org>
2013-08-16 09:41:51 +0930

    Initial Commit

---
+++ sU.php
@@ -0,0 +1,42 @@
+<?php
+class sU
+{
+	// Quick function to call print_r within a HTML <pre>
+	public static function debug($arr){
+		$backtrace = debug_backtrace();
+		$file = $backtrace[0]['file'];
+		$line = $backtrace[0]['line'];
+		
+		echo '<pre>';
+		echo "<b>$file:$line</b>\n";
+		print_r($arr);
+		echo '</pre>';
+	}
+
+	// Convert Bytes to prettier number
+	function bytesToSize($bytes, $precision = 2, $multiplier = 1024)
+	{  
+	    $kilobyte = $multiplier;
+	    $megabyte = $kilobyte * $multiplier;
+	    $gigabyte = $megabyte * $multiplier;
+	    $terabyte = $gigabyte * $multiplier;
+	   
+	    if (($bytes >= 0) && ($bytes < $kilobyte)) {
+	        return $bytes . ' B';
+	 
+	    } elseif (($bytes >= $kilobyte) && ($bytes < $megabyte)) {
+	        return round($bytes / $kilobyte, $precision) . ' KB';
+	 
+	    } elseif (($bytes >= $megabyte) && ($bytes < $gigabyte)) {
+	        return round($bytes / $megabyte, $precision) . ' MB';
+	 
+	    } elseif (($bytes >= $gigabyte) && ($bytes < $terabyte)) {
+	        return round($bytes / $gigabyte, $precision) . ' GB';
+	 
+	    } elseif ($bytes >= $terabyte) {
+	        return round($bytes / $terabyte, $precision) . ' TB';
+	    } else {
+	        return $bytes . ' B';
+	    }
+	}	
+}
