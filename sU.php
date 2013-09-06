31ef89b9669edffe651c22e777c1dc2421933af3
Sam Burney <sam.burney@air-stream.org>
2013-09-06 11:18:07 +0930

    Added object->array and array->object conversion functions

--- sU.php
+++ sU.php
@@ -38,5 +38,50 @@ class sU
 	    } else {
 	        return $bytes . ' B';
 	    }
-	}	
+	}
+	
+	/*
+	 * Function to Convert stdClass Objects to Multidimensional Arrays
+	 * http://www.if-not-true-then-false.com/2009/php-tip-convert-stdclass-object-to-multidimensional-array-and-convert-multidimensional-array-to-stdclass-object/
+	 */
+	public static function objectToArray($d) {
+		if (is_object($d)) {
+			// Gets the properties of the given object
+			// with get_object_vars function
+			$d = get_object_vars($d);
+		}
+ 
+		if (is_array($d)) {
+			/*
+			* Return array converted to object
+			* Using __FUNCTION__ (Magic constant)
+			* for recursive call
+			*/
+			return array_map(array('sU', 'objectToArray'), $d);
+		}
+		else {
+			// Return array
+			return $d;
+		}
+	}
+	 
+	/*
+	 * Function to Convert Multidimensional Arrays to stdClass Objects
+	 * http://www.if-not-true-then-false.com/2009/php-tip-convert-stdclass-object-to-multidimensional-array-and-convert-multidimensional-array-to-stdclass-object/
+	 */
+	public static function arrayToObject($d) {
+		if (is_array($d)) {
+			/*
+			* Return array converted to object
+			* Using __FUNCTION__ (Magic constant)
+			* for recursive call
+			*/
+			return (object) array_map(array('sU', 'objectToArray'), $d);
+		}
+		else {
+			// Return object
+			return $d;
+		}
+	}
+	 
 }
