19a5a4b8ea8093c77101f3b646cf7ccd5a9b0a95
Sam Burney <sam@burney.io>
2018-05-25 03:20:22 +0000

    Added vksprintf()

--- sU.php
+++ sU.php
@@ -194,5 +194,32 @@ class sU
 	    $firstOfMonth = strtotime(date("Y-m-01", $date));
 	    //Apply above formula.
 	    return intval(date("W", $date)) - intval(date("W", $firstOfMonth)) + 1;
-	}	
+	}
+	
+	/** 
+	* Like vsprintf, but accepts $args keys instead of order index. 
+	* Both numeric and strings matching /[a-zA-Z0-9_-]+/ are allowed. 
+	* 
+	* Example: vskprintf('y = %y$d, x = %x$1.1f', array('x' => 1, 'y' => 2)) 
+	* Result:  'y = 2, x = 1.0' 
+	* 
+	* $args also can be object, then it's properties are retrieved 
+	* using get_object_vars(). 
+	* 
+	* '%s' without argument name works fine too. Everything vsprintf() can do 
+	* is supported. 
+	* 
+	* @author Josef Kufner <jkufner(at)gmail.com> 
+	*/ 
+	public static function vksprintf($str, $args) 
+	{ 
+	    if (is_object($args)) { 
+	        $args = get_object_vars($args); 
+	    } 
+	    $map = array_flip(array_keys($args)); 
+	    $new_str = preg_replace_callback('/(^|[^%])%([a-zA-Z0-9_-]+)\$/', 
+	            function($m) use ($map) { return $m[1].'%'.($map[$m[2]] + 1).'$'; }, 
+	            $str); 
+	    return vsprintf($new_str, $args); 
+	} 
 }
